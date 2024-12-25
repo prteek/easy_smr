b092fdf7bf89c8824ea4e8d722baa0f576a8f3a7
prateek <prateek.patel@bricklane.com>
2024-12-25 22:00:21 +0000

    adding for training

---
+++ easy_smr/template/easy_smr_base/training/training.r
@@ -0,0 +1,19 @@
+library(here)
+library(renv)
+activate(here("replace_with_project_directory", "easy_smr_base"))
+# Import other libraries after this
+
+
+train <- function(input_data_path, model_save_path) {
+    # The function to execute the training.
+
+    # Args:
+    #   input_data_path: [character], input directory path where all the training file(s) reside in
+    #   model_save_path: [character], directory path to save your model(s)
+
+
+    # TODO: Write your modeling logic (including loading data)
+
+
+    # TODO: save the model(s) under 'model_save_path'
+}
